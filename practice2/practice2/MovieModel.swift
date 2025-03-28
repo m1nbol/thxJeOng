cdc68f901ed00af57c58c99987c11344b092aae0
Minbol <bmlee@postech.ac.kr>
2025-03-28 20:46:29 +0900

    📝 MovieModel

---
+++ practice2/practice2/MovieModel.swift
@@ -0,0 +1,16 @@
+//
+//  MovieModel.swift
+//  practice2
+//
+//  Created by BoMin LEE on 3/28/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieModel {
+    let movieImage: Image
+    let movieTitle: String
+    let movieLikeCount: Int
+    let movieReservationRate: Double
+}
