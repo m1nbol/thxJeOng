0a249d096126a7adac9e3a1ea27126ce3855f775
Minbol <bmlee@postech.ac.kr>
2025-03-28 19:22:21 +0900

    📝 CounterViewModel

---
+++ practice2/practice2/CounterViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CounterViewModel.swift
+//  practice2
+//
+//  Created by BoMin LEE on 3/28/25.
+//
+
+import SwiftUI
+
+class CounterViewModel: ObservableObject {
+    @Published var count: Int = 0
+}
