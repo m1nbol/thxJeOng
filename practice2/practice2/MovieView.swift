f4ec58fcce75567c3992f20447b0880bcc610506
Minbol <bmlee@postech.ac.kr>
2025-03-28 20:40:24 +0900

    📝 MovieView

---
+++ practice2/practice2/MovieView.swift
@@ -0,0 +1,23 @@
+//
+//  MovieView.swift
+//  practice2
+//
+//  Created by BoMin LEE on 3/28/25.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("HelloWorld")
+        }
+    }
+}
+
+#Preview {
+    MovieView()
+}
