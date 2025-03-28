0ab50d72d5e60031b1c8e2f1564395ea64d5daa7
Minbol <bmlee@postech.ac.kr>
2025-03-28 20:50:52 +0900

    📝 MovieViewModel

---
+++ practice2/practice2/MovieViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MovieViewModel.swift
+//  practice2
+//
+//  Created by BoMin LEE on 3/28/25.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class MovieViewModel {
+    let movieModel: [MovieModel] = [
+        .init(movieImage: .init(.mickey), movieTitle: "미키17", movieLikeCount: 972, movieReservationRate: 30.8),
+        .init(movieImage: .init(.toystory), movieTitle: "토이스토리", movieLikeCount: 999, movieReservationRate: 99.8),
+        .init(movieImage: .init(.brutalist), movieTitle: "브루탈리스트", movieLikeCount: 302, movieReservationRate: 24.8),
+        .init(movieImage: .init(.snowwhite), movieTitle: "백설공주", movieLikeCount: 302, movieReservationRate: 3.8),
+        .init(movieImage: .init(.whiplash), movieTitle: "위플래쉬", movieLikeCount: 604, movieReservationRate: 62.2),
+        .init(movieImage: .init(.conclave), movieTitle: "콘클라베", movieLikeCount: 392, movieReservationRate: 43.9),
+        .init(movieImage: .init(.thefall), movieTitle: "더폴", movieLikeCount: 30, movieReservationRate: 2.1)
+    ]
+}
