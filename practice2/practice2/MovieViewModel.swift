c3dc7d5ecba5fddc2f9ecb14d63508611257a42c
Minbol <bmlee@postech.ac.kr>
2025-03-28 20:52:51 +0900

    📝 MovieViewModel 수정
    이전 영화 함수 추가
    다음 영화 함수 추가

--- practice2/practice2/MovieViewModel.swift
+++ practice2/practice2/MovieViewModel.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 @Observable
 class MovieViewModel {
+    var currentIndex: Int = 0
+    
     let movieModel: [MovieModel] = [
         .init(movieImage: .init(.mickey), movieTitle: "미키17", movieLikeCount: 972, movieReservationRate: 30.8),
         .init(movieImage: .init(.toystory), movieTitle: "토이스토리", movieLikeCount: 999, movieReservationRate: 99.8),
@@ -19,4 +21,12 @@ class MovieViewModel {
         .init(movieImage: .init(.conclave), movieTitle: "콘클라베", movieLikeCount: 392, movieReservationRate: 43.9),
         .init(movieImage: .init(.thefall), movieTitle: "더폴", movieLikeCount: 30, movieReservationRate: 2.1)
     ]
+    
+    public func previousMovie() {
+        currentIndex = (currentIndex - 1 + movieModel.count) % movieModel.count
+    }
+    
+    public func nextMovie() {
+        currentIndex = (currentIndex + 1) % movieModel.count
+    }
 }
