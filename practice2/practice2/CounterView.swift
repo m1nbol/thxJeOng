6cc7b39bb4c378e3c25beee14eb07a81a9a0307f
Minbol <bmlee@postech.ac.kr>
2025-03-28 19:28:23 +0900

    📝 CounterView

---
+++ practice2/practice2/CounterView.swift
@@ -0,0 +1,28 @@
+//
+//  CounterView.swift
+//  practice2
+//
+//  Created by BoMin LEE on 3/28/25.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    @ObservedObject var viewModel: CounterViewModel = .init()
+    
+    var body: some View {
+        VStack {
+            Text("\(viewModel.count)")
+            Button {
+                viewModel.count += 1
+            } label: {
+                Text("카운트 증가 버튼")
+            }
+
+        }
+    }
+}
+
+#Preview {
+    CounterView()
+}
