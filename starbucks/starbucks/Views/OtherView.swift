e59f50ee18277738875998cff3fc7ec86c14c667
BoMin <bmlee@postech.ac.kr>
2025-03-30 00:01:53 +0900

    📝 더보기뷰 수정

---
+++ starbucks/starbucks/Views/OtherView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherView.swift
+//  starbucks
+//
+//  Created by BoMin Lee on 3/29/25.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text("OtherView")
+    }
+}
+
+#Preview {
+    OtherView()
+}
