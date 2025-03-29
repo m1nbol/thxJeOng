350c5dbe9796b711bd906dc994b17fa6cdd5195e
BoMin <bmlee@postech.ac.kr>
2025-03-29 23:36:41 +0900

    📝 starbucksApp 수정

--- starbucks/starbucks/starbucksApp.swift
+++ starbucks/starbucks/starbucksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct starbucksApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            LoginView(viewModel: LoginViewModel())
         }
     }
 }
