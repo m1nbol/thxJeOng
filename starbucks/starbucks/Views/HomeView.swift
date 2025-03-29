9177e93d322f8b3f54d39b9848ab5f6b4ba1d938
BoMin <bmlee@postech.ac.kr>
2025-03-30 00:01:45 +0900

    📝 홈뷰 생성

---
+++ starbucks/starbucks/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  starbucks
+//
+//  Created by BoMin Lee on 3/29/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+#Preview {
+    HomeView()
+}
