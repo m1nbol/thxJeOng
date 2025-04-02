30b3f087cd4e176e3c6bac2d3d4ef7e5ed3e4c3c
BoMin <bmlee@postech.ac.kr>
2025-04-02 20:44:18 +0900

    📝 오더뷰 생성

---
+++ starbucks/starbucks/Views/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  starbucks
+//
+//  Created by BoMin Lee on 4/2/25.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text("OrderView")
+    }
+}
+
+#Preview {
+    OrderView()
+}
