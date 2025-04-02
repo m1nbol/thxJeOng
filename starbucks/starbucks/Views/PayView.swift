654afb50cc7dab0a437efd8719773a5b38f2693c
BoMin <bmlee@postech.ac.kr>
2025-04-02 20:44:22 +0900

    📝 페이뷰 생성

---
+++ starbucks/starbucks/Views/PayView.swift
@@ -0,0 +1,18 @@
+//
+//  PayView.swift
+//  starbucks
+//
+//  Created by BoMin Lee on 4/2/25.
+//
+
+import SwiftUI
+
+struct PayView: View {
+    var body: some View {
+        Text("PayView")
+    }
+}
+
+#Preview {
+    PayView()
+}
