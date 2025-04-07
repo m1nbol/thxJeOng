57cb7a53057fd28511d15aa71cdcf2c1490dba30
BoMin <bmlee@postech.ac.kr>
2025-04-08 01:38:41 +0900

    ✍️ 로그인 뷰모델 수정
    로그인 기능 추가

--- starbucks/starbucks/ViewModels/LoginViewModel.swift
+++ starbucks/starbucks/ViewModels/LoginViewModel.swift
@@ -8,11 +8,26 @@
 import Foundation
 import SwiftUI
 
-@Observable
-class LoginViewModel {
+class LoginViewModel: ObservableObject {
     var loginModel: LoginModel = LoginModel(id: "", pw: "")
     
+    @AppStorage("emailStorage") private var savedEmail: String = ""
+    @AppStorage("passwordStorage") private var savedPassword: String = ""
+    
+    @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
+    
+    @Published var isLoginSuccess: Bool = false
+    @Published var loginErrorMessage: String?
+
     func login() {
-        print("ID: \(loginModel.id), PW: \(loginModel.pw)")
+        if loginModel.id == savedEmail && loginModel.pw == savedPassword {
+            isLoginSuccess = true
+            loginErrorMessage = nil
+            isLoggedIn = true
+            print("로그인 성공")
+        } else {
+            isLoginSuccess = false
+            loginErrorMessage = "이메일 또는 비밀번호가 일치하지 않습니다."
+        }
     }
 }
