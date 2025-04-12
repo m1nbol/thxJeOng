ddb7b34348003f7ce509c8a1b8c62e74ef8a0613
BoMin <bmlee@postech.ac.kr>
2025-04-12 16:11:26 +0900

    📔 4주차 생성

---
+++ practice4/practice4/practice4App.swift
@@ -0,0 +1,17 @@
+//
+//  practice4App.swift
+//  practice4
+//
+//  Created by BoMin Lee on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct practice4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
