5ebd4ea09ba2af81ef65e2fb980d27c5d1f79ecf
BoMin <bmlee@postech.ac.kr>
2025-04-15 16:29:09 +0900

    📝 원형이미지카드 변경
    - 프레임 설정 상위뷰에서 하는 거 적용

--- starbucks/starbucks/Views/Components/CircleImageCard.swift
+++ starbucks/starbucks/Views/Components/CircleImageCard.swift
@@ -10,16 +10,32 @@ import SwiftUI
 
 struct CircleImageCard: View {
     let menu: MenuModel
+    var fontSize: Int
     
     var body: some View {
         VStack(spacing: 10) {
             menu.image
                 .resizable()
                 .scaledToFit()
-                .frame(width: 130, height: 130) // frame 고정되면 안 되고 상위뷰에서 결정시켜야 함
-            Text(menu.name)
+                // .frame(width: 130, height: 130) // frame 고정되면 안 되고 상위뷰에서 결정시켜야 함
+                .frame(maxWidth: .infinity)
+            if fontSize == 14 {
+                Text(menu.name)
+                .font(.mainTextSemiBold14)
+                .frame(alignment: .center)
+            } else if fontSize == 13 {
+                Text(menu.name)
+                .font(.mainTextSemiBold13)
+                .frame(alignment: .center)
+            } else {
+                Text(menu.name)
                 .font(.mainTextLight14)
                 .frame(alignment: .center)
+            }
         }
     }
+}
+
+#Preview {
+    CircleImageCard(menu: MenuModel(id: 1, name: "에스프레소", image: Image(.espressoConpannaCircle)), fontSize: 14)
 }
