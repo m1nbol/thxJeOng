ba1407a9d6291086ff1a64c048b9fa6f03e817c1
BoMin <bmlee@postech.ac.kr>
2025-04-16 15:07:57 +0900

    ✨ 홈 화면 진입시 광고 표시

--- starbucks/starbucks/ViewModels/AdvertisementViewModel.swift
+++ starbucks/starbucks/ViewModels/AdvertisementViewModel.swift
@@ -8,14 +8,14 @@
 import Foundation
 import SwiftUI
 
-class AdvertisementViewModel: ObservableObject {
-    @Published var advertisementList: [AdvertisementModel] = []
-    
-    init() {
-        advertisementList = [
-            AdvertisementModel(image: Image(.whatsNewFirst), title: "25년 3월 일회용컵 없는 날 캠페인", content: "매월 10일은 일회용컵 없는 날! 스타벅스 에코매장에서 개인컵 및 다회용컵을 이용하세요."),
-            AdvertisementModel(image: Image(.whatsNewSecond), title: "스타벅스 ㅇㅇㅇ점을 찾습니다", content: "스타벅스 커뮤니티 스토어 파트너를 운영할 기관을 공모합니다."),
-            AdvertisementModel(image: Image(.whatsNewThird), title: "2월 8일, 리저브 스프링 신규 커피 출시", content: "산뜻하고 달콤한 풍미가 가득한 리저브를 맛보세요.")
-        ]
-    }
-}
+//class AdvertisementViewModel: ObservableObject {
+//    @Published var advertisementList: [AdvertisementModel] = []
+//    
+//    init() {
+//        advertisementList = [
+//            AdvertisementModel(image: Image(.whatsNewFirst), title: "25년 3월 일회용컵 없는 날 캠페인", content: "매월 10일은 일회용컵 없는 날! 스타벅스 에코매장에서 개인컵 및 다회용컵을 이용하세요."),
+//            AdvertisementModel(image: Image(.whatsNewSecond), title: "스타벅스 ㅇㅇㅇ점을 찾습니다", content: "스타벅스 커뮤니티 스토어 파트너를 운영할 기관을 공모합니다."),
+//            AdvertisementModel(image: Image(.whatsNewThird), title: "2월 8일, 리저브 스프링 신규 커피 출시", content: "산뜻하고 달콤한 풍미가 가득한 리저브를 맛보세요.")
+//        ]
+//    }
+//}
