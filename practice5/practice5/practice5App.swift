757b5b1dc02373712868f48ae58463ce8f548e89
BoMin <bmlee@postech.ac.kr>
2025-04-27 16:29:49 +0900

    📝 5주차 실습 프로젝트 생성

---
+++ practice5/practice5/practice5App.swift
@@ -0,0 +1,17 @@
+//
+//  practice5App.swift
+//  practice5
+//
+//  Created by BoMin Lee on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct practice5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
